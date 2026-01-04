22c6c94af345ae5da580538fb6d068efe24a69f6
SH Woo <imdlsrks.mc@gmail.com>
2026-01-04 14:56:39 +0900

    fix: make JwtProperties bindable

--- src/main/java/com/study/security/application/security/jwt/properties/JwtProperties.java
+++ src/main/java/com/study/security/application/security/jwt/properties/JwtProperties.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 @Getter
+@Setter
 @Component
 @Validated
 @ConfigurationProperties(prefix = "spring.security.jwt")
