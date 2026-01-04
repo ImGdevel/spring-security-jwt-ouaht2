50c4ca1527a30b6b78d3b7d0f34dc0bc316514c9
SH Woo <imdlsrks.mc@gmail.com>
2026-01-04 14:47:30 +0900

    refactor: make logout extensible via actions

--- src/main/java/com/study/security/application/security/local/handler/LogoutHandler.java
+++ src/main/java/com/study/security/application/security/local/handler/LogoutHandler.java
@@ -1,11 +1,11 @@
 package com.study.security.application.security.local.handler;
 
-import com.study.security.application.security.jwt.service.TokenBlacklistService;
-import com.study.security.application.security.jwt.provider.JwtCookieProvider;
+import com.study.security.application.security.common.logout.LogoutAction;
 import com.study.security.application.security.common.util.SecurityResponseSender;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -15,17 +15,15 @@ import org.springframework.stereotype.Component;
 public class LogoutHandler {
 
     private final SecurityResponseSender securityResponseSender;
-    private final JwtCookieProvider jwtCookieProvider;
-    private final TokenBlacklistService tokenBlacklistService;
+    private final List<LogoutAction> logoutActions;
 
     public void onLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         SecurityContextHolder.clearContext();
 
-        jwtCookieProvider.getRefreshTokenFromCookie(request)
-                .ifPresent(tokenBlacklistService::addToBlacklist);
-
-        jwtCookieProvider.deleteRefreshTokenCookie(response);
+        for (LogoutAction logoutAction : logoutActions) {
+            logoutAction.onLogout(request, response);
+        }
 
         securityResponseSender.sendSuccess(response, HttpServletResponse.SC_OK, "로그아웃되었습니다.");
     }
